7d7a4cf3bdd3aff356d17f764192dc6cfa76e5f5
minekoa <mineko.orange@gmail.com>
2019-03-11 10:06:19 +0900

    ispell-modeの設定(コミット生成ミスでうっかり消した、多分あってるはず）

--- init.el
+++ init.el
@@ -528,4 +528,12 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ispell mode (use aspell)
+
+(setq-default ispell-program-name "aspell")
+(eval-after-load "ispell"
+ '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ???
